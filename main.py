fd41bc082e83206212943ba4125e08fefcd9fa18
Athan <jonpuray@gmail.com>
2026-02-13 00:08:36 +0800

    new file

---
+++ main.py
@@ -0,0 +1,40 @@
+import os
+from fastapi import FastAPI
+from pymongo import MongoClient
+from pydantic import BaseModel
+from dotenv import load_dotenv
+from bson import ObjectId
+
+load_dotenv()
+
+app = FastAPI()
+
+# MongoDB connection
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["requests_db"]
+collection = db["users"]
+
+class User(BaseModel):
+    name: str
+    email: str
+
+@app.get("/")
+def home():
+    return {"status": "API is running"}
+
+@app.post("/users")
+def create_user(user: User):
+    result = collection.insert_one(user.dict())
+    return {
+        "message": "User created",
+        "id": str(result.inserted_id)
+    }
+
+@app.get("/users")
+def get_users():
+    users = []
+    for user in collection.find():
+        user["_id"] = str(user["_id"])
+        users.append(user)
+    return users
